bb2102a51ee5777b87f2891dcffa0ed68a5a71f9
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-21 02:15:13 +0900

    TASK1 : MainActivity -> SearchActivity로 변경.

--- app/src/main/java/escape/com/searchgitrepository/search/SearchActivity.kt
+++ app/src/main/java/escape/com/searchgitrepository/search/SearchActivity.kt
@@ -1,11 +1,12 @@
-package escape.com.searchgitrepository
+package escape.com.searchgitrepository.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import escape.com.searchgitrepository.R
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
     }
 }
