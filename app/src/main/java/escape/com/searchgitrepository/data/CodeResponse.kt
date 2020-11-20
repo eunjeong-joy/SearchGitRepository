9aa55226e105e99f3a15bd50b5d9f5fb09013244
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-21 01:50:57 +0900

    TASK2 : CodeResponse와 Repository data class 추가.

---
+++ app/src/main/java/escape/com/searchgitrepository/data/CodeResponse.kt
@@ -0,0 +1,18 @@
+package escape.com.searchgitrepository.data
+
+import java.util.*
+
+data class SearchResponse(
+    val totalCount: Int = 0,
+    val repositories: List<Repository>
+)
+
+data class Repository (
+    val name: String = "",
+    val fullName: String = "",
+    val htmlUrl: String = "",
+    val description: String = "",
+    val stargazersCount: Int = 0,
+    val pushedAt: Long = 0,
+    val language: String = ""
+)
