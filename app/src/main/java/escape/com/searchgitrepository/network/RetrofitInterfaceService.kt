d1cb892a42efa6d1711a92d038b13faba0974b7a
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-21 15:31:01 +0900

    request 파라미터 추가.

--- app/src/main/java/escape/com/searchgitrepository/network/RetrofitInterfaceService.kt
+++ app/src/main/java/escape/com/searchgitrepository/network/RetrofitInterfaceService.kt
@@ -3,11 +3,17 @@ package escape.com.searchgitrepository.network
 import escape.com.searchgitrepository.data.SearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface RetrofitInterfaceService {
 
     @GET("/search/repositories")
-    fun requestSearchRepository(): Call<SearchResponse>
+    fun requestSearchRepository(
+        @Header("Authorization") token:String,
+        @Query("q") keyword: String,
+        @Query("per_page") perPage: Int? = 20,
+        @Query("page") page: Int? = 1
+    ): Call<SearchResponse>
 
 }
