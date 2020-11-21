f68dfd39d55920db827b1181bf3c327c85bf6dd2
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-21 17:49:53 +0900

    TASK4 : Response, InterfaceService 이름 변경.

--- app/src/main/java/escape/com/searchgitrepository/network/RepositoryInterfaceService.kt
+++ app/src/main/java/escape/com/searchgitrepository/network/RepositoryInterfaceService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
-interface RetrofitInterfaceService {
+interface RepositoryInterfaceService {
 
     @GET("/search/repositories")
     fun requestSearchRepository(
