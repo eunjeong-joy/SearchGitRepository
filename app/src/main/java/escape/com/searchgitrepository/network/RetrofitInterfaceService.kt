52ba8d524a1ef59b2b7fb12be92a6a83baf41ecf
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-21 15:28:43 +0900

    access token 제거.

--- app/src/main/java/escape/com/searchgitrepository/network/RetrofitInterfaceService.kt
+++ app/src/main/java/escape/com/searchgitrepository/network/RetrofitInterfaceService.kt
@@ -7,7 +7,6 @@ import retrofit2.http.Headers
 
 interface RetrofitInterfaceService {
 
-    @Headers("Authorization: 59d75bb4281d1e78a660d537c72be49ce6e6e56b")
     @GET("/search/repositories")
     fun requestSearchRepository(): Call<SearchResponse>
 
